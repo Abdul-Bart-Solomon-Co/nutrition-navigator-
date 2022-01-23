a8fec8df797430eea15ce1b1f01a8a5e74d468c6
Solomon <solo.serry@gmail.com>
2022-01-22 21:35:48 -0500

    added an array

--- src/App.js
+++ src/App.js
@@ -51,6 +51,9 @@ function App() {
         }
       }).then((res) => {
         console.log(res.data)
+       
+        const someArray = ["nf_calories", "nf_dietary_fiber", "nf_protein", "nf_saturated_fat", "nf_sugars", "nf_total_carbhydrate", "nf_total_fat", "nf_sodium", "full_nutrients", "food_name", "brand_name", "photo",]
+       
       })
     }
   }, [foodItemName])
